3ca7d769939495847c9c4433e8a2965420657ca1
Lustiyana <Lustiyana1801@gmail.com>
2023-08-20 17:07:46 +0700

    Add classes from wa bot

--- app.py
+++ app.py
@@ -14,31 +14,6 @@ from keras_preprocessing import image as keras_img
 
 app = Flask(__name__)
 
-class_labels = ['Afrinaldi', 'Alia Fadhila', 'Andi Malia Fadilah Bahari', 'Ashrul Nasrulloh', 'Azhar', 'Aziz Nuzul Praramadhana', 'Faiq Fadhlurrahman El Hakim', 'Farhan Rizky', 'Fawzan Ibnu Fajar', 'Fellia Ayu S A', 'Ifany Dewi Tustianti', 'Imam Firdaus', 'Intan Permata Sari', 'Laela Chintia Alviani ', 'Lustiyana', 'Muhamad Rizki Isa Darmawan', 'Muhammad Fahmi Rizaldi Ilham', 'Muhammad Gilang Nur Haliz', 'Mujahid Ansori Majid', 'Naufal Rizqullah', 'Nurul Aulia Dewi', 'Raihan Adam', 'Rifaldo Sukma Hidayat', 'Siti Haerani', 'Soniawan', 'fahriz']
-
-model = Sequential()
-model.add(Conv2D(32, kernel_size = (3, 3), activation='relu', input_shape=(224,224,3)))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(BatchNormalization())
-model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(BatchNormalization())
-model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(BatchNormalization())
-model.add(Conv2D(96, kernel_size=(3,3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(BatchNormalization())
-model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(BatchNormalization())
-model.add(Dropout(0.2))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-#model.add(Dropout(0.3))
-model.add(Dense(len(class_labels),activation='softmax'))
-model.load_weights('./model/model.h5')
-
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -70,10 +45,32 @@ def extract_face(image, required_size=(160, 160)):
 
 @app.route('/process', methods=['POST'])
 def process():
-    # data_classes = get_data()
-    # print(data_classes)
-    # list_classes = list(data_classes)
-    # print(list_classes)
+    form_data = request.form
+    specific_key = form_data.get('classes') 
+    class_labels= specific_key.split(';')
+
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size = (3, 3), activation='relu', input_shape=(224,224,3)))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(96, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(len(class_labels),activation='softmax'))
+    model.load_weights('./model/model_5.h5')
+    
     image = request.files['image']
     img = Image.open(image)
     plt.imshow(img)
@@ -93,27 +90,7 @@ def process():
     images = np.vstack([x])
     pred = model.predict(images, batch_size=32)
     predicted_class = class_labels[np.argmax(pred)]
-    print(jsonify({'prediction': predicted_class}))
     return jsonify({'prediction': predicted_class})
-    # return get_data()
-
-def get_data():
-    api_url='https://strapi.lustiyana18.my.id/api/users?sort[0]=name'
-    try:
-        response = requests.get(api_url)
-        if response.status_code == 200:
-            data = response.json()
-            return jsonify(data)
-        else:
-            return jsonify({'error': 'Failed to fetch data from API'}), response.status_code
-    
-    except requests.exceptions.RequestException as e:
-        return jsonify({'error': 'Error occurred during API request'}), 500
-
-    # data={
-    #     'image': '/static/face_image.png'
-    # }
-    # return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
