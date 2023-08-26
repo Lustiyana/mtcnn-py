176b91d0bfaa5dcbe99897dfa99780186174d1e7
Lustiyana <Lustiyana1801@gmail.com>
2023-08-27 03:29:40 +0700

    Add image checking

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import requests
 from keras.models import Sequential
 from keras.layers import Conv2D,MaxPooling2D,Dense,Flatten,Dropout, BatchNormalization
 from keras_preprocessing import image as keras_img
+import urllib.request
 
 app = Flask(__name__)
 
@@ -49,6 +50,7 @@ def process():
     specific_key = form_data.get('classes') 
     class_labels= specific_key.split(';')
     # class_labels = ['Adi Pratama Putra', 'Afrinaldi', 'Aka Fadila', 'Alia Fadhila', 'Ammar Taradifa', 'Andi Malia Fadilah Bahari', 'Arief Raihan Nur Rahman', 'Arif Muhamad Iqbal ', 'Ashrul Nasrulloh', 'Azhar', 'Aziz Nuzul Praramadhana', 'Bella Arsylia ', 'Elis Kartika', 'Faiq Fadhlurrahman El Hakim', 'Farhan Rizky', 'Fawzan Ibnu Fajar', 'Fellia Ayu S A', 'Gani', 'Hilman Suhendar', 'Ifany Dewi Tustianti', 'Ilham Rizky Agustin ', 'Imam Firdaus', 'Indah Sri Lestari', 'Indri Nurfiani', 'Intan Permata Sari', 'Iqbal Putra Ramadhan', 'Jalalul', 'Laela Chintia Alviani ', 'Lustiyana', 'Moch Apip Tanuwijaya', 'Moch Arsyil Albany', 'Mochamad Najib Budi Noosrsyahbannie', 'Muhamad Farid Fauzi', 'Muhamad Iqbal Setiawan', 'Muhamad Rizki Isa Darmawan', 'Muhammad Afian Anwar', 'Muhammad Alwy Solehudin', 'Muhammad Fahmi Rizaldi Ilham', 'Muhammad Gilang Nur Haliz', 'Mujahid Ansori Majid', 'Naufal Rizqullah', 'Nurdila Farha Kamila', 'Nurul Aulia Dewi', 'Raihan Adam', 'Rifaldo Sukma Hidayat', 'Siti Haerani', 'Siti Yayah Rokayah ', 'Soniawan', 'fahriz']
+
 
     model = Sequential()
     model.add(Conv2D(32, kernel_size = (3, 3), activation='relu', input_shape=(224,224,3)))
@@ -93,6 +95,45 @@ def process():
     except Exception:
         return jsonify({'error': 'Absen gagal! Tidak ada wajah yang terdeteksi, coba lagi', 'success': False})
 
+
+def are_images_equal(image_path1, image_path2):
+    urllib.request.urlretrieve(image_path2, 'image.jpeg')
+    image1 = Image.open(image_path1)
+    image2 = Image.open('image.jpeg')
+
+    if image1.size != image2.size:
+        return False
+
+    pixel_data1 = list(image1.getdata())
+    pixel_data2 = list(image2.getdata())
+
+    if pixel_data1 != pixel_data2:
+        return False
+
+    return True
+
+@app.route('/image-checking', methods=['POST'])
+def image_checking():
+    form_data = request.form
+    image_path1 = request.files['image']
+    specific_key = form_data.get('image_array')
+    if len(specific_key) == 0:
+        return jsonify({'success':True, 'message': ''})
+        
+    image_array= specific_key.split(';')
+
+    result = []
+    for image_path2 in image_array:
+        if are_images_equal(image_path1, image_path2):
+            result.append(True)
+        else:
+            result.append(False)
+
+
+    if any(result):
+        return jsonify({'success':False, 'message':'Gambar yang sudah diupload tidak boleh diupload kembali'})
+    else:
+        return jsonify({'success':True, 'message': ''})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
